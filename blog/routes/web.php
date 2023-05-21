768f6b7d66b4c3640c4645e9c4adaa545579ef7f
gary31136 <37773492+gary31136@users.noreply.github.com>
2023-05-22 00:20:59 +0800

    try the vite to use the scss

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,7 +12,26 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// www.everyonecan.website/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/posts', function () {
+    return view('posts.list');
+});
+
+// /posts/9487
+Route::get('/posts/{id}', function ($id) {
+
+    return view('posts.show');
 });
